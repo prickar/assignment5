a1085778a7c8351cd200f3dc61d322658c95def5
Paola <paola.vidal@edu.cmeducations.se>
2023-01-24 11:40:31 +0100

    about site and projects site

--- js/aboutjs.js
+++ js/aboutjs.js
@@ -1,24 +1,37 @@
 $(function(){
 
-    $(".me-image").on("click", function() {
-        $(this).siblings("p").slideToggle(500);
+      // find the div.fade elements and hook the hover event
+  $('div.fade').hover(function() {
+    // on hovering over, find the element we want to fade *up*
+    var fade = $('> div', this);
     
-        let opacityValue = $(this).hasClass("active") ? 1 : 0.5;
-    
-        $(this).parent().siblings().fadeTo(800, opacityValue)
-    
-        $(this).toggleClass("active");
-    
-    }) 
+    // if the element is currently being animated (to a fadeOut)...
+    if (fade.is(':animated')) {
+      // ...take it's current opacity back up to 1
+      fade.stop().fadeTo(250, 1);
+    } else {
+      // fade in quickly
+      fade.fadeIn(250);
+    }
+  }, function () {
+    // on hovering out, fade the element out
+    var fade = $('> div', this);
+    if (fade.is(':animated')) {
+      fade.stop().fadeTo(3000, 0);
+    } else {
+      // fade away slowly
+      fade.fadeOut(3000);
+    }
+  });
 
-    $(".work-image").on("click", function() {
-        $(this).siblings("p").slideToggle(500);
-    
-        let opacityValue = $(this).hasClass("active") ? 1 : 0.5;
-    
-        $(this).parent().siblings().fadeTo(800, opacityValue)
-    
-        $(this).toggleClass("active");
-    
-    })
+  $(".click-image").on("click", function() {
+    $(this).siblings("p").slideToggle(500);
+
+    let opacityValue = $(this).hasClass("active") ? 1 : 0.5;
+
+    $(this).parent().siblings().fadeTo(800, opacityValue)
+
+    $(this).toggleClass("active");
+
+}) 
 })
